17134d7840e92505698c223e6b5f4c95f3610b32
pakohappy <pako_happy@msn.com>
2025-03-30 11:27:22 +0200

    prueba conexión

--- bot.py
+++ bot.py
@@ -5,15 +5,23 @@ from configuracion.config_loader import ConfigLoader
 from log.log_loader import setup_logging
 import logging
 
+# Cargar configuraciones desde config_loader.
+config = ConfigLoader('configuracion\config.ini')
+
+# Datos de conexión a MetaTrader 5.
+login = config.get_int('metatrader', 'login')
+password = config.get('metatrader', 'password')
+server = config.get('metatrader', 'server')
+
 # Configurar logging desde logging_loader
 setup_logging()
 
 logging.info("Iniciando el script de trading...")
 
 # Iniciar conexión con MetaTrader 5
-if not mt5.initialize():
+if not mt5.initialize(login=login, password=password, server=server):
+    # Si no se puede conectar, registrar el error y cerrar la conexión.
     logging.error("Error al inicializar MetaTrader 5")
-    print("Error al inicializar MetaTrader 5")
     mt5.shutdown()
 else:
     logging.info("MetaTrader 5 inicializado correctamente.")
