68bbd448eb98565df74d3c3c07934406872b382c
pakohappy <pako_happy@msn.com>
2025-04-14 22:35:14 +0200

    probando pycharm

--- gitgo.py
+++ gitgo.py
@@ -1,23 +1,24 @@
 import subprocess
 import argparse
+from colorama import Fore
 
 def git_push(commit_message):
     try:
         # Ejecutar "git add --all"
         subprocess.run(["git", "add", "--all"], check=True)
-        print(">>> Archivos añadidos al índice.")
+        print(Fore.GREEN + ">>> Archivos añadidos al índice.")
 
         # Ejecutar "git commit -m 'mensaje'"
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
-        print(f">>> Commit realizado con mensaje: '{commit_message}'")
+        print(Fore.GREEN + f">>> Commit realizado con mensaje: '{commit_message}'")
 
         # Ejecutar "git push"
         subprocess.run(["git", "push"], check=True)
-        print(">>> Cambios enviados al repositorio remoto.")
+        print(Fore.GREEN + ">>> Cambios enviados al repositorio remoto.")
     except subprocess.CalledProcessError as e:
-        print(f">>> Error al ejecutar el comando: {e}")
+        print(Fore.RED + f">>> Error al ejecutar el comando: {e}")
     except Exception as e:
-        print(f">>> Error inesperado: {e}")
+        print(Fore.RED + f">>> Error inesperado: {e}")
 
 if __name__ == "__main__":
     # Configurar argparse para recibir el mensaje del commit como argumento
