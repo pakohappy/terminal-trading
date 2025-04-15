86481b087ab2a64312dbbd96675374665e9ad5f3
pakohappy <pako_happy@msn.com>
2025-04-15 19:20:07 +0200

    pelango con pycharm

--- lib_bot/robot1.py
+++ lib_bot/robot1.py
@@ -1,28 +1,53 @@
-# -*- coding: utf-8 -*-
-
+"""
+-*- coding: utf-8 -*-
+"""
 # Robot - 1.
 # MACD - Moving Average Convergence Divergence.
 
 import MetaTrader5 as mt5
 import pandas as pd
-from datetime import datetime
 import time
-
 from indicadores.tendencia import Tendencia
 import logging
 
+def initialize_mt5():
+    """
+    Inicializa la conexión con MetaTrader 5.
+    """
+    if not mt5.initialize():
+        error_code = mt5.last_error()
+        logging.error(f"Failed to initialize MetaTrader 5, error code = {error_code}")
+        quit()
+
+def obterner_df(symbol: str, timeframe: int, ult_velas: int) -> pd.DataFrame:
+    """
+    Obiene el DataFrame de las últimas velas(ult_velas).
+    """
+    # Obtener los precios de las últimas velas.
+    rates = mt5.copy_rates_from_pos(symbol, timeframe, 0, ult_velas)
+    rates_df = pd.DataFrame(rates)
+
+    # Convertir la columna 'time' a formato datetime.
+    rates_df['time'] = pd.to_datetime(rates_df['time'], unit='s')
+    rates_df['time'] = rates_df['time'].dt.tz_localize('UTC').dt.tz_convert('Europe/Madrid')
+    rates_df['time'] = rates_df['time'].dt.strftime('%d-%m-%Y %H:%M:%S')
+
+    return rates_df
+
 class Robot1:
-    def __init__(self, cofigpath='configuracion/config.ini'):
+    """
+    Clase Robot 1.
+    """
+    DEFAULT_CONFIG_PATH = 'configuracion/config.ini'
+
+    def __init__(self, cofig_path=DEFAULT_CONFIG_PATH):
         """
         Inicializa el robot con la configuración y parámetros necesarios.
         """
         # Inicializa la conexión con MetaTrader 5.
-        if not mt5.initialize():
-            print("initialize() failed, error code =",mt5.last_error())
-            quit()
-        logging.info("ROBOT1 - Conectando con MetaTrader 5...")
+        initialize_mt5()
 
-        self.config_path = cofigpath
+        self.config_path = cofig_path
         self.symbol = 'EURUSD'
         self.timeframe = mt5.TIMEFRAME_M1
         self.volumen = 0.1
@@ -37,20 +62,10 @@ class Robot1:
         self.periodo_senyal = 9
         self.df = None
 
-
-    def obterner_df(self, symbol, timeframe, ult_velas) -> pd.DataFrame:
-        # Obtener los precios de las últimas velas.
-        rates = mt5.copy_rates_from_pos(symbol, timeframe, 0, ult_velas)
-        rates_df = pd.DataFrame(rates)
-
-        # Convertir la columna 'time' a formato datetime.
-        rates_df['time'] = pd.to_datetime(rates_df['time'], unit='s')
-        rates_df['time'] = rates_df['time'].dt.tz_localize('UTC').dt.tz_convert('Europe/Madrid')
-        rates_df['time'] = rates_df['time'].dt.strftime('%d-%m-%Y %H:%M:%S')
-
-        return rates_df
-
-    def abrir_orden(self, symbol, volumen, senyal):
+    def abrir_orden(self, symbol: str, volumen: float, senyal: str):
+        """
+        Función para abrir una orden.
+        """
         tick = mt5.symbol_info_tick(symbol)
 
         order_dict = {'buy': 0, 'sell': 1}
@@ -74,21 +89,24 @@ class Robot1:
 
         return order_result
 
-    def ejecutar(self):
+    def ejecutar(self) -> None:
+        """
+        Bucle principal del Robot 1.
+        """
         while True:
             try:
                 # Comprobar si hay posiciones abiertas o se ha alcanzado el máximo de posiciones.
                 self.posiciones_abiertas = mt5.positions_total()
                 logging.info(f"ROBOT1 - Posiciones abiertas: {self.posiciones_abiertas}")
             except Exception as e:
-                logging.error(f"ROBOT1 - Error al obtener las posiciones abiertas.")
+                logging.error(f"ROBOT1 - Error al obtener las posiciones abiertas: {e}")
 
             if self.posiciones_abiertas >= self.max_posiciones:
                 logging.info("ROBOT1 - Máximo de posiciones abiertas alcanzado.")
             else:
                 try:
                     # Obtener el DataFrame de precios.
-                    self.df = self.obterner_df(self.symbol, self.timeframe, self.ult_velas)
+                    self.df = obterner_df(self.symbol, self.timeframe, self.ult_velas)
                     logging.info("ROBOT1 - Datos obtenidos desde MetaTrader 5.")
                 except Exception as e:
                     logging.error(f"ROBOT1 - Error al obtener datos: {e}")
