4b739f336d5d5985572706a2d7a63cf862c28d87
pakohappy <pako_happy@msn.com>
2025-05-06 13:26:43 +0200

    sl_dinamic

--- configuracion/config_loader.py
+++ configuracion/config_loader.py
@@ -2,6 +2,57 @@
 import configparser
 import logging
 
+
+def escribir_configuracion():  # TODO: Añadir un try-except para manejar errores al escribir el archivo.
+    """
+    Permite al usuario modificar la configuración del robot.
+    """
+    try:
+        config_path = "configuracion/config.ini"
+        config = configparser.ConfigParser()
+        config.read(config_path)
+
+        print("\n### Modificar Configuración ###")
+        print("Secciones disponibles:", list(config.sections()))
+        seccion = input(">>> Ingresa la sección que deseas modificar: ").strip()
+        if seccion not in config:
+            print(">>> La sección no existe.")
+            return
+
+        print("Claves disponibles en la sección:", list(config[seccion].keys()))
+        clave = input(">>> Ingresa la clave que deseas modificar: ").strip()
+        if clave not in config[seccion]:
+            print(">>> La clave no existe.")
+            return
+
+        nuevo_valor = input(f">>> Ingresa el nuevo valor para '{clave}': ").strip()
+        config[seccion][clave] = nuevo_valor
+
+        # Guardar los cambios en el archivo
+        with open(config_path, "w") as configfile:
+            config.write(configfile)
+
+        logging.info(f"Configuración actualizada: [{seccion}] {clave} = {nuevo_valor}")
+        print(f">>> Configuración actualizada: [{seccion}] {clave} = {nuevo_valor}")
+    except Exception as e:
+        logging.error(f"Error al escribir la configuración.{e}")
+
+
+def leer_configuracion() -> None:
+    """
+    Muestra las configuraciones actuales del archivo config.ini.
+    """
+    config_path = "configuracion/config.ini"
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    print("\n### Configuración Actual ###")
+    for seccion in config.sections():
+        print(f"[{seccion}]")
+        for clave, valor in config[seccion].items():
+            print(f"{clave} = {valor}")
+        print()
+
 class ConfigLoader:
     def __init__(self, config_file='config.ini'):
         self.config = configparser.ConfigParser()
@@ -22,67 +73,3 @@ class ConfigLoader:
     def get_boolean(self, section, key):
         """Obtiene un valor booleano."""
         return self.config.getboolean(section, key)
-    
-# ## Ejemplo de uso:
-# from config_loader import ConfigLoader
-
-# # Cargar configuraciones
-# config = ConfigLoader('config.ini')
-
-# # Acceder a valores específicos
-# api_key = config.get('general', 'api_key')
-# symbol = config.get('trading', 'symbol')
-# lot_size = config.get_float('trading', 'lot_size')
-
-# print(f"API Key: {api_key}")
-# print(f"Trading Symbol: {symbol}")
-# print(f"Lot Size: {lot_size}")
-
-    def escribir_configuracion(self): # TODO: Añadir un try-except para manejar errores al escribir el archivo.
-        """
-        Permite al usuario modificar la configuración del robot.
-        """
-        try:
-            config_path = "configuracion/config.ini"
-            config = configparser.ConfigParser()
-            config.read(config_path)
-
-            print("\n### Modificar Configuración ###")
-            print("Secciones disponibles:", list(config.sections()))
-            seccion = input(">>> Ingresa la sección que deseas modificar: ").strip()
-            if seccion not in config:
-                print(">>> La sección no existe.")
-                return
-
-            print("Claves disponibles en la sección:", list(config[seccion].keys()))
-            clave = input(">>> Ingresa la clave que deseas modificar: ").strip()
-            if clave not in config[seccion]:
-                print(">>> La clave no existe.")
-                return
-
-            nuevo_valor = input(f">>> Ingresa el nuevo valor para '{clave}': ").strip()
-            config[seccion][clave] = nuevo_valor
-
-            # Guardar los cambios en el archivo
-            with open(config_path, "w") as configfile:
-                config.write(configfile)
-
-            logging.info(f"Configuración actualizada: [{seccion}] {clave} = {nuevo_valor}")
-            print(f">>> Configuración actualizada: [{seccion}] {clave} = {nuevo_valor}")
-        except:
-            logging.error(f"Error al escribir la configuración.")
-
-    def leer_configuracion(self):
-        """
-        Muestra las configuraciones actuales del archivo config.ini.
-        """
-        config_path = "configuracion/config.ini"
-        config = configparser.ConfigParser()
-        config.read(config_path)
-
-        print("\n### Configuración Actual ###")
-        for seccion in config.sections():
-            print(f"[{seccion}]")
-            for clave, valor in config[seccion].items():
-                print(f"{clave} = {valor}")
-            print()
