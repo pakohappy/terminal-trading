0701e7ad44525cb3cae3c70c0254a9ebd93655e7
pakohappy <pako_happy@msn.com>
2025-05-10 15:52:43 +0200

    tendencia/sma

--- indicadores/tendencia.py
+++ indicadores/tendencia.py
@@ -67,6 +67,9 @@ class Tendencia:
         # Determinar la tendencia
         self.df['tendencia'] = self.df['close'] > self.df['sma']
 
+        #df_sin_nan = df_sin_nan.reset_index(drop=True)
+        #todo reset index.
+
         # Obtener la última tendencia
         ultima_tendencia = self.df['tendencia'].iloc[-1]
 
