5652471864a1198d83d157581d625f2dcfddbd4b
pakohappy <pako_happy@msn.com>
2025-05-22 11:21:51 +0200

    robot1.py

--- robot1.py
+++ robot1.py
@@ -5,7 +5,6 @@ import MetaTrader5 as mt5
 from log.log_loader import setup_logging
 from platform.Metaquotes import Metaquotes as mtq
 from indicators.Oscillator import Oscillator
-import logging
 
 """
 Configuración de Robot 1.
@@ -35,15 +34,23 @@ def run():
 
     mtq.initialize_mt5()
 
+    while True:
+        positions = mt5.positions_get(symbol=SYMBOL)
 
-    df = mtq.get_df(SYMBOL, TIMEFRAME, LAST_CANDLES)
-    print(df)
-    logging.info(f"ROBOT1 - Datos obtenidos desde MetaTrader 5.")
-    indicator = Oscillator(df)
-    signal = indicator.stochastic(K_PERIOD, D_PERIOD, SMOOTH_K, OVERBOUGHT_LEVEL, OVERSOLD_LEVEL, MODE)
+        if positions is None:
+            df = mtq.get_df(SYMBOL, TIMEFRAME, LAST_CANDLES)
+            print(df)
+            print(f"ROBOT1 - Datos obtenidos desde MetaTrader 5.")
+            indicator = Oscillator(df)
+            signal = indicator.stochastic(K_PERIOD, D_PERIOD, SMOOTH_K, OVERBOUGHT_LEVEL, OVERSOLD_LEVEL, MODE)
 
-    if signal == 0 or signal == 1:
-        mtq.open_order(SYMBOL, VOLUME, signal, PIPS_SL, PIPS_TP, DEVIATION, COMMENT)
-    else:
-        print("No hay signal.")
-    return None
+            if signal == 0 or signal == 1:
+                mtq.open_order(SYMBOL, VOLUME, signal, PIPS_SL, PIPS_TP, DEVIATION, COMMENT)
+            else:
+                print("No hay signal.")
+
+        elif len(positions) > 0:
+            print(f"Total positions on {SYMBOL}: ", len(positions))
+            # display all open positions
+            for position in positions:
+                print(position)
