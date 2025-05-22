2be90dc1a18b937e9d0489c176f85790072c9dc8
pakohappy <pako_happy@msn.com>
2025-05-22 10:56:34 +0200

    robot1.py

--- robot1.py
+++ robot1.py
@@ -18,6 +18,14 @@ PIPS_SL = 50
 PIPS_TP = 100
 DEVIATION = 100
 
+# Stochastic.
+K_PERIOD = 5
+D_PERIOD = 3
+SMOOTH_K = 3
+OVERBOUGHT_LEVEL = 80
+OVERSOLD_LEVEL = 20
+MODE = 0
+
 # Importamos la configuración del logging.
 setup_logging()
 
@@ -28,7 +36,8 @@ def run():
     df = mtq.get_df(SYMBOL, TIMEFRAME, LAST_CANDLES)
     print(df)
     logging.info(f"ROBOT1 - Datos obtenidos desde MetaTrader 5.")
-    signal = Oscillator.stochastic(df)
+    indicator = Oscillator(df)
+    signal = indicator.stochastic(K_PERIOD, D_PERIOD, SMOOTH_K, OVERBOUGHT_LEVEL, OVERSOLD_LEVEL, MODE)
 
     if signal == 0 or signal == 1:
         mtq.open_order(SYMBOL, VOLUME, signal, PIPS_SL, PIPS_TP, DEVIATION)#todo solucionar portabilidad Metaquotes.
