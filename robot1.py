5a47fa2aa1e34f9522b4ca32655e9bbbb8fb48f2
pakohappy <pako_happy@msn.com>
2025-05-29 09:10:02 +0200

    robot1.py

--- robot1.py
+++ robot1.py
@@ -14,17 +14,17 @@ SYMBOL = 'USDJPY'
 TIMEFRAME = mt5.TIMEFRAME_M5
 VOLUME = 0.01
 LAST_CANDLES = 30
-PIPS_SL = 100
-PIPS_TP = 150
+PIPS_SL = 50
+PIPS_TP = 70
 DEVIATION = 100
 COMMENT = "Robot 1 Order"
 
 # Stochastic.
-K_PERIOD = 7
+K_PERIOD = 5
 D_PERIOD = 3
 SMOOTH_K = 3
-OVERBOUGHT_LEVEL = 70
-OVERSOLD_LEVEL = 30
+OVERBOUGHT_LEVEL = 80
+OVERSOLD_LEVEL = 20
 MODE = 0
 
 # Importamos la configuración del logging.
@@ -54,32 +54,32 @@ def run():  #todo hacer que no cierre las posiciones en el cambio de tendencia, 
             else:
                 print(">>> No hay signal.")
 
-        elif len(positions) == 1:
-            for position in positions:
-                df = mtq.get_df(SYMBOL, TIMEFRAME, LAST_CANDLES)
-                indicator = Oscillator(df)
-                signal = indicator.stochastic(K_PERIOD, D_PERIOD, SMOOTH_K, OVERBOUGHT_LEVEL, OVERSOLD_LEVEL, MODE)
+        # elif len(positions) == 1:
+        #     for position in positions:
+        #         df = mtq.get_df(SYMBOL, TIMEFRAME, LAST_CANDLES)
+        #         indicator = Oscillator(df)
+        #         signal = indicator.stochastic(K_PERIOD, D_PERIOD, SMOOTH_K, OVERBOUGHT_LEVEL, OVERSOLD_LEVEL, MODE)
+        #
+        #         if position.type == 0 and signal == 2 or position.type == 1 and signal == 1:
+        #             print(">>> No hay señal para abrir una segunda posición.")
+        #
+        #         if position.type == 0 and signal == 1:
+        #             mtq.open_order_sell(SYMBOL, VOLUME, signal, PIPS_SL, PIPS_TP, DEVIATION, COMMENT)
+        #
+        #         if position.type == 1 and signal == 2:
+        #             mtq.open_order_buy(SYMBOL, VOLUME, signal, PIPS_SL, PIPS_TP, DEVIATION, COMMENT)
 
-                if position.type == 0 and signal == 2 or position.type == 1 and signal == 1:
-                    print(">>> No hay señal para abrir una segunda posición.")
-
-                if position.type == 0 and signal == 1:
-                    mtq.open_order_sell(SYMBOL, VOLUME, signal, PIPS_SL, PIPS_TP, DEVIATION, COMMENT)
-
-                if position.type == 1 and signal == 2:
-                    mtq.open_order_buy(SYMBOL, VOLUME, signal, PIPS_SL, PIPS_TP, DEVIATION, COMMENT)
-
-        elif len(positions) > 0:
-            for position in positions:
-
-                df = mtq.get_df(SYMBOL, TIMEFRAME, LAST_CANDLES)
-                indicator = Oscillator(df)
-                signal = indicator.stochastic(K_PERIOD, D_PERIOD, SMOOTH_K, OVERBOUGHT_LEVEL, OVERSOLD_LEVEL, MODE)
-
-                if position.type == 0 and signal == 1 or position.type == 1 and signal == 2:
-                    mtq.close_position(position)
-                else:
-                    print ("No hay signal que marque el cierre de la posición.")
+        # elif len(positions) > 0:
+        #     for position in positions:
+        #
+        #         df = mtq.get_df(SYMBOL, TIMEFRAME, LAST_CANDLES)
+        #         indicator = Oscillator(df)
+        #         signal = indicator.stochastic(K_PERIOD, D_PERIOD, SMOOTH_K, OVERBOUGHT_LEVEL, OVERSOLD_LEVEL, MODE)
+        #
+        #         if position.type == 0 and signal == 1 or position.type == 1 and signal == 2:
+        #             mtq.close_position(position)
+        #         else:
+        #             print ("No hay signal que marque el cierre de la posición.")
 
         else:
             print("No hay posiciones abiertas.")
